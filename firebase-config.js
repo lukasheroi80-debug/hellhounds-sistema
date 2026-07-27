9c4cbcc00b0e5ef282b2389ffd4c11a9169f0c19
lukasheroi80-debug <lukasheroi80@gmail.com>
2026-07-27 13:04:44 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,14 @@
 // normalmente, mas salva apenas no navegador de cada pessoa.
 //
 // Tutorial no arquivo README.md.
-window.HELLHOUNDS_FIREBASE_CONFIG = null;
+window.HELLHOUNDS_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDMoXyscoLUvixYpCXhBQrwi3RnUz2hjkY",
+  authDomain: "hellhounds-sistema.firebaseapp.com",
+  projectId: "hellhounds-sistema",
+  storageBucket: "hellhounds-sistema.firebasestorage.app",
+  messagingSenderId: "889652080867",
+  appId: "1:889652080867:web:9c676771839eb6d58e55d6"
+};
 
 /*
 window.HELLHOUNDS_FIREBASE_CONFIG = {
